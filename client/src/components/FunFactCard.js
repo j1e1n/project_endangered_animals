c027ea99d7966bc4eab04a6e93c98e65e571dcef
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-02-27 13:17:23 +0000

    fun fact card component set up with boilerplate code

--- client/src/components/FunFactCard.js
+++ client/src/components/FunFactCard.js
@@ -1,1 +1,14 @@
-// fun fact card
+import React from 'react';
+
+const FunFactCard = () => {
+    return(
+        <>
+        <div className="fun-fact-card">
+
+        </div>
+        </>
+    )
+};
+
+
+export default FunFactCard;
