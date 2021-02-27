5294f67ea3265dd0eefdf587f2f6c590ff237dc4
Gregor Simpson <catkelly@Catrionas-MacBook-Pro.local>
2021-02-27 16:30:25 +0000

    updated file

--- client/src/components/AnimalsList.js
+++ client/src/components/AnimalsList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AnimalsList = ({animals, selectedAnimals}) => {
+const AnimalsList = ({animals, selectedAnimal}) => {
 
     const animalListItems = animals.map((picture, index) => {
         return <li onClick={() => {onSelectedAnimal(picture)}} 
