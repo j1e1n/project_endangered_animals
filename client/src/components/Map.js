d65a76e22e798de070856adc4d0f920ef6d5703c
CodeScott00 <dougal_00@hotmail.com>
2021-02-27 13:42:56 +0000

    Scott first commit for new features, files populated with boilerplate stuff

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,1 +1,25 @@
-// map
+import React from 'react';
+import map from '../placeholder-map.jpeg'
+
+const Map = () => {
+
+    
+    
+    
+    
+    
+    return(
+        <>
+        <div className='map'>
+        <h1>Interactive Map</h1>
+        <img src={'../placeholder-map.jpeg'}>Map</img>
+
+
+        </div>
+        </>
+
+    );
+};
+
+
+export default Map;
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,1 +1,25 @@
-// quiz
+import React from 'react';
+import quiz from '../quiz.png'
+
+const Quiz = () => {
+
+    
+    
+    
+    
+    
+    return(
+        <>
+        <div className='quiz'>
+        <h1>Quiz</h1>
+        <img src={'../quiz.png'}>Quiz</img>
+
+
+        </div>
+        </>
+
+    );
+};
+
+
+export default Quiz;
