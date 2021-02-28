82ca1f1f4ef009686983b0af497dc45a525c270c
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-02-28 17:03:19 +0000

    added box with red list categories, shows when hovering over animal status

--- client/src/components/AnimalDetail.js
+++ client/src/components/AnimalDetail.js
@@ -7,21 +7,43 @@ const animalDetail = ({ selectedAnimal, onFavouriteClick }) => {
     if (!selectedAnimal) {
         return (null)
     }
+
+  
+
     return (
         <>
             <div className="animal-detail">
                 <h2>{selectedAnimal.animal_name}</h2>
                 <p>Scientific name: {selectedAnimal.species}</p>
-                <br></br>
-                <p>The {selectedAnimal.animal_name} is <b>{selectedAnimal.status}</b>, there are {selectedAnimal.population} left in the world. </p>
-                <br></br>
+                <p>There are {selectedAnimal.population} {selectedAnimal.animal_name} left in the world.</p>
+                <div className="myDIV">
+                    <h3><b>{selectedAnimal.status}</b></h3>
+                    
+                </div>   
+                <div className="hide">
+                    <h4>IUCN Red List Categories</h4>
+                    <ul>
+                        <li><b>Extinct</b> - the species no longer exists</li>
+                        <li><b>Extinct in the Wild</b> - survives only in captivity</li>
+                        <li><b>Critically Endangered</b> - in an extremely critical state</li>
+                        <li><b>Endangered</b> - very high risk of extinction in the wild</li>
+                        <li><b>Vulnerable</b> - high risk of human-caused extinction without further intervention</li>
+                        <li><b>Near Threatened</b> - close to being at high risk of extinction in the near future</li>
+                        <li><b>Least Concern</b> - unlikely to become extinct in the near future</li>
+                    </ul>
+                </div>
+
+
                 <img src={selectedAnimal.pictures} width="300" height="300" alt={selectedAnimal.animal_name}/>
+                
                 <p>Locations: {selectedAnimal.locations}</p>
                 <p>Habitat: {selectedAnimal.habitat}</p>
                 <p>{selectedAnimal.description}</p>
-                <br />
                 <li onClick={() => { onFavouriteClick(selectedAnimal) }}  >Add to Favourite</li>
             </div>
+
+            
+           
         </>
     )
 };
