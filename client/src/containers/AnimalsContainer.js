2194d16917770d61f4b200279721e4756ddbb444
CodeScott00 <dougal_00@hotmail.com>
2021-02-28 10:33:41 +0000

    import updates

--- client/src/containers/AnimalsContainer.js
+++ client/src/containers/AnimalsContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Favourites from '../components/Favourites';
+
 
 import animalDetail from '../components/AnimalDetail'
 import AnimalsList from '../components/AnimalsList'
