46c2a5222b844c247cd7b0d4469eafe1b7ec6c2d
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-03-01 11:37:35 +0000

    added get questions, on start button click

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,8 +1,34 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 
 const Quiz = () => {
 
+    const [questions, setQuestions] = usestate([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answers, setAnswers] = useState([]);
+
+    // get questions
+    const getQuestions = () => {
+        console.log("getting questions")
+        fetch('http://localhost:5000/api/quiz')
+        .then(res => res.json())
+        .then(data => setQuestions(data))
+    }
+
+    // use effect
+    useEffect(() => {
+        getQuestions()
+    }, [])
+
+
+    //on start button click
+    const onStartClick = () => {
+        setCurrentQuestion(questions[0].Question)
+        setAnswers(currentQuestion.answerOptions)
+    }
+
+    // handle answer click
+
+   
     
     
     
